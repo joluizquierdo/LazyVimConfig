51885a8d1c64675c57de08ed1c5a7154cf618ad2
Jolu Izquierdo <22649229+joluizquierdo@users.noreply.github.com>
2025-12-01 12:39:42 +0100

    feat(neo-tree): disable plugin

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,6 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-        },
-      },
-    },
+    enabled = false,
   },
 }
